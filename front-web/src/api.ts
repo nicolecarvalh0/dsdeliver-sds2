5720a181993623076b2cbac4c695cb083af3e041
Nicole <nicolecarvalho.ti@hotmail.com>
2021-01-10 13:44:53 -0300

    Add env vars

--- front-web/src/api.ts
+++ front-web/src/api.ts
@@ -3,6 +3,7 @@ import { OrderPayLoad } from "./Orders/types";
 
 const API_URL = process.env.REACT_APP_API_URL;
 
+
 const mapboxToken = process.env.REACT_APP_ACCESS_TOKEN_MAP_BOX;
 
 export function fetchProducts() {
